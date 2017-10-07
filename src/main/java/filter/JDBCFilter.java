81cba4e5d9f3ce44f4068c566398a9f55f343916
OksanaShumei <kgajxel@gmail.com>
2017-10-08 00:31:13 +0300

    Update JDBCFilter.java

--- src/main/java/filter/JDBCFilter.java
+++ src/main/java/filter/JDBCFilter.java
@@ -30,18 +30,12 @@ public class JDBCFilter implements Filter {
     // Проверить является ли Servlet цель текущего request?
     private boolean needJDBC(HttpServletRequest request) {
         System.out.println("JDBC Filter");
-        //
-        // Servlet Url-pattern: /spath/*
-        //
-        // => /spath
         String servletPath = request.getServletPath();
-        // => /abc/mnp
         String pathInfo = request.getPathInfo();
 
         String urlPattern = servletPath;
 
         if (pathInfo != null) {
-            // => /spath/*
             urlPattern = servletPath + "/*";
         }
 
@@ -49,8 +43,6 @@ public class JDBCFilter implements Filter {
         // Value: ServletRegistration
         Map<String, ? extends ServletRegistration> servletRegistrations = request.getServletContext()
                 .getServletRegistrations();
-
-        // Коллекционировать все Servlet в вашем WebApp.
         Collection<? extends ServletRegistration> values = servletRegistrations.values();
         for (ServletRegistration sr : values) {
             Collection<String> mappings = sr.getMappings();
@@ -66,11 +58,7 @@ public class JDBCFilter implements Filter {
             throws IOException, ServletException {
 
         HttpServletRequest req = (HttpServletRequest) request;
-
-        // Открыть  connection (соединение) только для request со специальной ссылкой.
-        // (Например ссылка к servlet, jsp, ..)
-        // Избегать открытия Connection для обычных запросов.
-        // (Например image, css, javascript,... )
+        
         if (this.needJDBC(req)) {
 
             System.out.println("Open Connection for: " + req.getServletPath());
@@ -86,7 +74,6 @@ public class JDBCFilter implements Filter {
                 MyUtils.storeConnection(request, conn);
 
                 // Разрешить request продвигаться далее.
-                // (Далее к следующему Filter tiếp или к цели).
                 chain.doFilter(request, response);
 
                 // Вызвать метод commit() чтобы завершить транзакцию с DB.
@@ -103,7 +90,6 @@ public class JDBCFilter implements Filter {
         // не нужно открывать connection.
         else {
             // Разрешить request продвигаться далее.
-            // (Далее к следующему Filter tiếp или к цели).
             chain.doFilter(request, response);
         }
 
